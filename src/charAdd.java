9dbe921a81d8b4b64bbb7108f49691ead694ec41
huangyingw@linux_programming <huangyingw@gmail.com>
2011-12-30 10:52:12 +0800

    n

---
+++ src/charAdd.java
@@ -0,0 +1,34 @@
+public class charAdd {
+	static int tranNum(char x) {
+		if (x > '9' || x < '0')
+			return -1;
+		x -= '0';
+		return x;
+	}
+
+	static int atoi(String x) {
+		int result = 0;
+		boolean positive = true;
+		int nav = 0;
+		if (x.charAt(0) == '-' || x.charAt(0) == '_') {
+			positive = false;
+			nav++;
+		}
+		do {
+			result += tranNum(x.charAt(nav++));
+			if (0 != x.charAt(nav))
+				result *= 10;
+		} while (nav < x.length()-1);
+		result += tranNum(x.charAt(nav++));
+		if (!positive)
+			result = -result;
+		return result;
+	}
+
+	public static void main(String arg[]) {
+		String source1 = "-234";
+		System.out.println(atoi(source1));
+		String source2 = "235";
+		System.out.println(atoi(source2));
+	}
+}
